aff66fdb01bdcea0320a57d5da4d8404a68872b5
Oleksandr Veselov <alexander.veselov.dev@gmail.com>
2024-08-24 01:30:43 +0300

    Implement RenderPipeline

--- src/application.py
+++ src/application.py
@@ -1,5 +1,39 @@
 import pygame
+import threading
 from stable_diffusion import StableDiffusion
+
+class RenderPipeline:
+    def __init__(self, app):
+        # TODO: don't depend from application
+        self.app = app
+        self.running = False
+
+    def render(self):
+        self.running = True
+        self.reneder_welcome_screen()
+        while self.running:
+            # TODO: separate renderer from image generation
+            image_data = self.app.image_generator.generate(self.app.prompt, self.app.negative_prompt)
+            self.render_image(image_data)
+
+    def render_image(self, image_data):
+        image = pygame.image.load(image_data)
+        image = pygame.transform.smoothscale(image, self.app.render_size)
+        if self.app.rotate:
+            image = pygame.transform.rotate(image, 90)
+        if self.app.flip:
+            image = pygame.transform.flip(image, flip_x=self.app.rotate, flip_y=not self.app.rotate)
+        self.app.screen.blit(image, (0, 0))
+
+    def reneder_welcome_screen(self):
+        pink = (255, 192, 203)
+        white = (255, 255, 255)
+        self.app.screen.fill(pink)
+        font = pygame.font.SysFont("Cantarell", 74)
+        text = font.render("Hello nya!", True, white)
+        text_rect = text.get_rect()
+        text_rect.center = (self.app.screen.get_width() // 2, self.app.screen.get_height() // 2.5)
+        self.app.screen.blit(text, text_rect)
 
 class Application:
     def __init__(self, params):
@@ -10,51 +44,26 @@ class Application:
         self.prompt = params.prompt
         self.negative_prompt = params.negative_prompt
         self.render_size = (params.height, params.width) if params.rotate else (params.width, params.height)
-        self.image_generator = StableDiffusion(params.ip, params.port, self.render_size)
         self.screen = pygame.display.set_mode(
             (params.width, params.height),
             pygame.FULLSCREEN if params.windowed else 0
         )
+        self.image_generator = StableDiffusion(params.ip, params.port, self.render_size)
+        self.render_pipeline = RenderPipeline(self)
+        self.render_thread = threading.Thread(target=self.render_pipeline.render, daemon=True)
         pygame.mouse.set_visible(False)
         pygame.display.set_caption('AI Photo Frame')
 
     def run(self):
-        self.reneder_welcome_screen()
-        pygame.display.flip()
-
         self.running = True
+        self.render_thread.start()
         while self.running:
             self.process_events(pygame.event.get())
-            self.render()
-
+            pygame.display.flip()
         pygame.quit()
         return 0
-    
-    def render(self):
-        image_data = self.image_generator.generate(self.prompt, self.negative_prompt)
-        self.render_image(image_data)
-        pygame.display.flip()
     
     def process_events(self, events):
         for event in events:
             if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONUP:
                 self.running = False
-        
-    def render_image(self, image_data):
-        image = pygame.image.load(image_data)
-        image = pygame.transform.smoothscale(image, self.render_size)
-        if self.rotate:
-            image = pygame.transform.rotate(image, 90)
-        if self.flip:
-            image = pygame.transform.flip(image, flip_x=self.rotate, flip_y=not self.rotate)
-        self.screen.blit(image, (0, 0))
-        
-    def reneder_welcome_screen(self):
-        pink = (255, 192, 203)
-        white = (255, 255, 255)
-        self.screen.fill(pink)
-        font = pygame.font.SysFont("Cantarell", 74)
-        text = font.render("Hello nya!", True, white)
-        text_rect = text.get_rect()
-        text_rect.center = (self.screen.get_width() // 2, self.screen.get_height() // 2.5)
-        self.screen.blit(text, text_rect)
