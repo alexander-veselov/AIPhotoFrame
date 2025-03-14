d9de7ed0e12b0a505ff59afa921e5c7c6f817f3b
Oleksandr Veselov <alexander.veselov.dev@gmail.com>
2025-03-14 20:06:15 +0200

    Don't update Inky display if the image hasn't changed

--- src/display/inky_display.py
+++ src/display/inky_display.py
@@ -1,11 +1,12 @@
 import pygame
-from PIL import Image
+from PIL import Image, ImageChops
 from inky.auto import auto
 
 class InkyDisplay:
     def __init__(self, width, height):
         self.surface = pygame.Surface((width, height))
         self.display = auto()
+        self.last_image = None
 
     def get_surface(self):
         return self.surface
@@ -14,8 +15,13 @@ class InkyDisplay:
         pixel_data = pygame.image.tostring(self.surface, 'RGB')
         image = Image.frombytes('RGB', self.surface.get_size(), pixel_data)
         image = image.crop((0, 0, *self.display.resolution))
-        self.display.set_image(image)
-        self.display.show()
+        if self.last_image is None:
+            self.last_image = image
+        diff = ImageChops.difference(self.last_image, image)
+        if diff.getbbox():
+            self.last_image = image
+            self.display.set_image(image)
+            self.display.show()
     
     def reset(self):
         pass # TODO: implement if needed
