66afaf5bc64bb43ef68620a951639edce3ba895a
Oleksandr Veselov <alexander.veselov.dev@gmail.com>
2025-08-26 21:25:23 +0300

    Extend mock data with vertical images

--- src/generators/mock_generator.py
+++ src/generators/mock_generator.py
@@ -3,12 +3,19 @@ from io import BytesIO
 class MockGenerator:
     def __init__(self):
         self.image_index = 0
-        self.images = []
-        for image_path in ["image1.bin", "image2.bin"]:
-            with open("data/" + image_path, 'rb') as file:
-                image_data = file.read()
-                self.images.append(image_data)
+        self.images = {"horizontal":[], "vertical": []}
+        self._append("horizontal", "1.bin")
+        self._append("horizontal", "2.bin")
+        self._append("vertical", "1.bin")
+        self._append("vertical", "2.bin")
+    
+    def _append(self, type, image_name):
+        with open("data/" + type + "/" + image_name, 'rb') as file:
+            image_data = file.read()
+            self.images[type].append(image_data)
 
     def generate(self, size, prompt, negative_prompt=""):
-        self.image_index = (self.image_index + 1) % len(self.images)
-        return BytesIO(self.images[self.image_index]), ""
+        type = "horizontal" if size[0] > size[1] else "vertical"
+        images = self.images[type]
+        self.image_index = (self.image_index + 1) % len(images)
+        return BytesIO(images[self.image_index]), ""
